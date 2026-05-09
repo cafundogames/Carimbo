ceb7f84e95437fe2e17b6bafaeb3218e16b1bdb0
kodiitulip <kodii.tulip@proton.me>
2026-05-09 14:43:20 -0300

    ran the formatter on it sorry '-'

--- source/scenes/components/stamped_animated_sprite/shaded_animated_sprite_3d.gd
+++ source/scenes/components/stamped_animated_sprite/shaded_animated_sprite_3d.gd
@@ -2,19 +2,25 @@
 class_name ShadedAnimatedSprite3D
 extends AnimatedSprite3D
 
+signal burned
+
 const SHADER_STAMPABLE_SPRITE: ShaderMaterial = preload("uid://brb72iyoqsyk6")
 const BURN_SFX = preload("uid://dfe4v2sdvd1bd")
 
-signal burned
 @export var audio_player: AudioStreamPlayer
-
 @export var stamp_viewport: SubViewport:
-	set(v): stamp_viewport = v; _update_viewport_size(); update_configuration_warnings()
+	set = set_stamp_viewport
+@export var stamp_viewport_texture: ViewportTexture:
+	set = set_stamp_viewport_texture
+
 
 func _init() -> void:
-	if not Engine.is_editor_hint(): return
-	if not material_override: material_override = SHADER_STAMPABLE_SPRITE.duplicate()
-	if not sprite_frames: sprite_frames = SpriteFrames.new()
+	if not Engine.is_editor_hint():
+		return
+	if not material_override:
+		material_override = SHADER_STAMPABLE_SPRITE.duplicate()
+	if not sprite_frames:
+		sprite_frames = SpriteFrames.new()
 
 
 func _ready() -> void:
@@ -27,16 +33,37 @@ func _ready() -> void:
 	_update_shader_anim()
 
 
+func _get_configuration_warnings() -> PackedStringArray:
+	var war: PackedStringArray = []
+	if not stamp_viewport:
+		war.append("Select a SubViewport node for the stamp effect")
+	return war
+
+
+func set_stamp_viewport(new: SubViewport) -> void:
+	stamp_viewport = new
+	_update_viewport_size()
+	update_configuration_warnings()
+
+
+func set_stamp_viewport_texture(new: ViewportTexture) -> void:
+	stamp_viewport_texture = new
+	if not stamp_viewport:
+		return
+	var material: ShaderMaterial = material_override
+	material.set_shader_parameter.call_deferred(&"texture_carimbo", stamp_viewport_texture)
+
+
 ## "Carimba" uma imagem em algum lugar aleatório da imagem
 func stamp(image: Texture2D, size: Vector2 = Vector2(1.0, 1.0)) -> void:
 	var vp_size = stamp_viewport.size
 
 	# anda até 1/4 do tamanho do viewport pra algum lado
-	var r = vp_size/4.0
+	var r = vp_size / 4.0
 	var pos_offset = Vector2(randi_range(-r.x, r.x), randi_range(-r.y, r.y))
 	var rot = deg_to_rad(randf_range(-50.0, 50.0))
 
-	var pos = vp_size/2.0 + pos_offset
+	var pos = vp_size / 2.0 + pos_offset
 	stamp_offset(image, pos, size, rot)
 
 
@@ -52,18 +79,56 @@ func stamp_offset(image: Texture2D, pos: Vector2, size: Vector2, rot: float) -> 
 	# Faz o viewport atualizar no próximo frame
 	stamp_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
 	# Apaga o sprite depois do fim do próximo frame
-	get_tree().process_frame.connect(func():
-		get_tree().process_frame.connect(sprite.queue_free, CONNECT_ONE_SHOT),
-	CONNECT_ONE_SHOT)
+	get_tree().process_frame.connect(
+		func():
+			get_tree().process_frame.connect(sprite.queue_free, CONNECT_ONE_SHOT),
+		CONNECT_ONE_SHOT,
+	)
+
+
+## Executa o efeito de burn
+func trigger_burn_fx(sound: bool = true, burn_time: float = 1.0) -> void:
+	if sound and audio_player:
+		audio_player.reparent(get_tree().current_scene)
+		audio_player.stream = BURN_SFX
+		audio_player.finished.connect(audio_player.queue_free, CONNECT_ONE_SHOT)
+		audio_player.play()
+	var particles: GPUParticles3D = Consts.PARTICLE_SPARK.instantiate()
+	get_tree().current_scene.add_child(particles)
+	particles.global_position = global_position
+	if not particles.finished.is_connected(particles.queue_free):
+		particles.finished.connect(particles.queue_free, CONNECT_ONE_SHOT)
+	particles.emitting = true
+
+	var burn_tween: Tween = create_tween()
+	burn_tween.tween_property(
+		material_override,
+		^"shader_parameter/burn_amount",
+		1.0,
+		burn_time,
+	).from(0.0)
+	burn_tween.finished.connect(burned.emit)
+
+
+## Chamada quando a hitbox leva dano para carimbar o sprite
+func on_hitbox_damaged(info: AttackInfo) -> void:
+	stamp(info.stamp_texture if info.stamp_texture else Consts.CARIMBO, info.stamp_size)
+
+
+func clear_viewport() -> void:
+	stamp_viewport.render_target_clear_mode = SubViewport.CLEAR_MODE_ONCE
+	stamp_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
+
 
 ## Atualiza o tamanho do viewport de carimbos de acordo com a animação
 func _update_viewport_size() -> void:
-	if not sprite_frames or not stamp_viewport: return
+	if not sprite_frames or not stamp_viewport:
+		return
 	var max_x: int = 256
 	var max_y: int = 256
 
 	# Itera por cada frame de cada animação
-	if sprite_frames: for anim in sprite_frames.get_animation_names():
+	if sprite_frames:for anim in sprite_frames.get_animation_names():
 		for f in sprite_frames.get_frame_count(anim):
 			var tex = sprite_frames.get_frame_texture(anim, f)
 			max_x = max(max_x, tex.get_width())
@@ -78,7 +143,8 @@ func _update_viewport_size() -> void:
 
 ## Atualiza a texture no shader de acordo com a animação atual
 func _update_shader_anim() -> void:
-	if not material_override: return
+	if not material_override:
+		return
 	var material: ShaderMaterial = material_override
 
 	var curr_frame = sprite_frames.get_frame_texture(animation, frame)
@@ -99,40 +165,3 @@ func _update_shader_anim() -> void:
 		material.set_shader_parameter("region_rect", Vector4(0.0, 0.0, 1.0, 1.0))
 
 	material.set_shader_parameter("texture_albedo", curr_frame)
-
-
-## Executa o efeito de burn
-func trigger_burn_fx(sound: bool = true, burn_time: float = 1.0) -> void:
-	if sound and audio_player:
-		audio_player.reparent(get_tree().current_scene)
-		audio_player.stream = BURN_SFX
-		audio_player.finished.connect(audio_player.queue_free, CONNECT_ONE_SHOT)
-		audio_player.play()
-	var particles: GPUParticles3D = Consts.PARTICLE_SPARK.instantiate()
-	get_tree().current_scene.add_child(particles)
-	particles.global_position = global_position
-	if not particles.finished.is_connected(particles.queue_free):
-		particles.finished.connect(particles.queue_free, CONNECT_ONE_SHOT)
-	particles.emitting = true
-
-	var burn_tween: Tween = create_tween()
-	burn_tween.tween_property(material_override, ^"shader_parameter/burn_amount", 1.0, burn_time
-		).from(0.0)
-	burn_tween.finished.connect(burned.emit)
-
-
-## Chamada quando a hitbox leva dano para carimbar o sprite
-func on_hitbox_damaged(info: AttackInfo) -> void:
-	stamp(info.stamp_texture if info.stamp_texture else Consts.CARIMBO, info.stamp_size)
-
-
-func _get_configuration_warnings() -> PackedStringArray:
-	var war: PackedStringArray = []
-	if not stamp_viewport:
-		war.append("Select a SubViewport node for the stamp effect")
-	return war
-
-
-func clear_viewport() -> void:
-	stamp_viewport.render_target_clear_mode = SubViewport.CLEAR_MODE_ONCE
-	stamp_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
